9e5358b1b7936e6dff8d27cde5432b9863df4b01
simlal <simlalonde@hotmail.com>
2022-11-03 11:13:17 -0400

    Add get request brewery info

--- fetch_data.py
+++ fetch_data.py
@@ -1,10 +1,36 @@
-import urllib.request
+import requests
 import json
 import yaml
+# import time
 
 # Accessing vars from yaml
 with open('env.yaml') as f:
     env = yaml.safe_load(f)
 client_id = env['client_id']
 client_secret = env['client_secret']
-baseURL = env['untappd_v4']
+baseURL = env['apiURL']
+brewery_id = env['brewery_id']
+
+def get_brewery_info() :
+    endpoint = '/v4/brewery/info/' + str(brewery_id)
+    payload = {
+        'client_id' : client_id,
+        'client_secret' : client_secret,
+        'BREWERY_ID' : brewery_id
+    }
+    print('Fetching Brewery info data')
+    resp = requests.get(baseURL+endpoint,params=payload)
+    try :
+        resp_dict = resp.json()
+    except :
+        print('Response could not be serialized')
+    info = json.dumps(resp_dict, indent=4)
+    print(info)
+endpoint = '/v4/brewery/info/'
+payload = {
+    'client_id' : client_id,
+    'client_secret' : client_secret,
+    'BREWERY_ID' : brewery_id
+}
+
+get_brewery_info()
